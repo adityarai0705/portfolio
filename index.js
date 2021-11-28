4027a12c9832a5edc4982c1b9709769fc554bf0e
Aditya Raj Rai <44937487+adityarai0705@users.noreply.github.com>
2021-11-28 17:23:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+function open_instagram(){
+    window.open( "https://www.instagram.com/aditya.rai0705/", 'self');
+}
+function open_facebook(){
+    window.open( "https://www.facebook.com/profile.php?id=100073061290543", 'self');
+}
+function open_github(){
+    window.open( "https://github.com/adityarai0705", 'self');
+}
+function open_codeforces(){
+    window.open( "https://codeforces.com/profile/aditya.rai0705", 'self');
+}
+function open_linkedin(){
+    window.open( "NotFound.html", 'self');
+}
+function homepage(){
+    window.open( "index.html", 'self');
+}
+
+
+
+function scroll_left(){
+    document.getElementById('interest_223').scrollLeft -= 100;
+}
+function scroll_right(){
+    document.getElementById('interest_223').scrollLeft += 100;
+}
